70fb1de45c486e2577bbdf7e7a8c8cc44fb8393f
DaLae37 <dalae37@gmail.com>
2017-08-29 11:35:15 +0900

    기초 세팅

---
+++ DropOutSchool/Assets/Scripts/mainScene/mainSceneMgr.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class mainSceneMgr : MonoBehaviour {
+    public void selectScene()
+    {
+        SceneManager.LoadScene("selectScene");
+    }
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ DropOutSchool/Assets/Scripts/selectScene/selectSceneMgr.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class selectSceneMgr : MonoBehaviour {
+    public void mainScene()
+    {
+        SceneManager.LoadScene("mainScene");
+    }
+    public void Stage1()
+    {
+        SceneManager.LoadScene("Stage1");
+    }
+}
