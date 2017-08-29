337551f629efc2b1f3ec710fb2ca3b7c8272a5fc
DaLae37 <dalae37@gmail.com>
2017-08-30 00:37:21 +0900

    이동 구현

---
+++ DropOutSchool/Assets/Scripts/Stage1/CameraCtrl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCtrl : MonoBehaviour {
+    public static CameraCtrl instance;
+    public Transform PlayerPos;
+    public float turnAmount = 90;
+    public float turnSpeed = 100;
+    public bool cameraRot;
+    public bool left;
+	// Use this for initialization
+	void Start () {
+        cameraRot = false;
+        instance = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (cameraRot)
+        {
+            float turnDig = turnSpeed * Time.deltaTime;
+            turnAmount -= turnDig;
+            transform.Rotate(new Vector3(0, 0, turnDig));
+            if ((turnAmount <= 0 && !left) || (turnAmount >= 0 && left))
+            {
+                cameraRot = false;
+                transform.Rotate(new Vector3(0, 0, turnAmount));
+            }
+        }
+	}
+    private void FixedUpdate()
+    {
+        transform.position = new Vector3(PlayerPos.position.x, PlayerPos.position.y, -10);
+    }
+}
--- DropOutSchool/Assets/Scripts/Stage1/PlayerCtrl.cs
+++ DropOutSchool/Assets/Scripts/Stage1/PlayerCtrl.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerCtrl : MonoBehaviour {
-
+    private Vector2 touchPosition;
+    private float swipeResistance = 200.0f;
+    public float Speed = 5.0f;
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +13,50 @@ public class PlayerCtrl : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
+        {
+            touchPosition = Input.mousePosition;
+        }
+        if (Input.GetMouseButtonUp(0) || Input.GetMouseButtonUp(1))
+        {
+            float swipeForceX = touchPosition.x - Input.mousePosition.x;
+            float swipeForceY = touchPosition.y - Input.mousePosition.y;
+            if (Mathf.Abs(swipeForceX) >= swipeResistance && Mathf.Abs(swipeForceX) >= Mathf.Abs(swipeForceY))
+            {
+                if(swipeForceX < 0)
+                    Turn(-90);
+                else
+                    Turn(90);
+            }
+            else if(Mathf.Abs(swipeForceY) >= swipeResistance && Mathf.Abs(swipeForceX) <= Mathf.Abs(swipeForceY))
+            {
+                if(swipeForceY < 0)
+                {
+                    TurnTime();
+                }
+            }
+        }
+    }
+    void FixedUpdate()
+    {
+        transform.Translate(new Vector3(0, Speed, 0) * Time.smoothDeltaTime);
+    }
+    public void Turn(int amount)
+    {
+        if (!CameraCtrl.instance.cameraRot)
+        {
+            transform.Rotate(new Vector3(0, 0, amount));
+            if (amount < 0)
+                CameraCtrl.instance.left = true;
+            else
+                CameraCtrl.instance.left = false;
+            CameraCtrl.instance.cameraRot = true;
+            CameraCtrl.instance.turnAmount = amount;
+            CameraCtrl.instance.turnSpeed = amount;
+        }
+    }
+    public void TurnTime()
+    {
+
+    }
 }
