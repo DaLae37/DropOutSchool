ced0570053b0aaac31cf7e984a7a4f364ff39a43
DaLae37 <dalae37@gmail.com>
2017-09-27 16:06:55 +0900

    튜토리얼 세팅

---
+++ DropOutSchool/Assets/Scripts/tutorialScene/tutorialSceneMgr.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class tutorialSceneMgr : MonoBehaviour {
+
+    public GameObject genderPoP;
+    public GameObject gender;
+    public GameObject namePoP;
+    public GameObject name;
+    public GameObject FemaleImage;
+    public GameObject MaleImage;
+    public Text nameText;
+    bool isFemale;
+	// Use this for initialization
+	void Start () {
+        isFemale = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Female()
+    {
+        isFemale = true;
+        unHideGenderPopUp();
+    }
+    public void Male()
+    {
+        isFemale = false;
+        unHideGenderPopUp();
+    }
+    public void Yes()
+    {
+        if (isFemale)
+        {
+            PlayerPrefs.SetInt("isFemale", 1);
+            FemaleImage.SetActive(true);
+        }
+        else
+        {
+            PlayerPrefs.SetInt("isFemale", 0);
+            MaleImage.SetActive(true);
+        }
+        HideGenderPopUp();
+        gender.SetActive(false);
+        name.SetActive(true);
+    }
+    public void HideGenderPopUp()
+    {
+        genderPoP.SetActive(false);
+    }
+    public void unHideGenderPopUp()
+    {
+        genderPoP.SetActive(true);
+    }
+    public void HideNamePop()
+    {
+        namePoP.SetActive(false);
+    }
+    public void unHideNamePop()
+    {
+        namePoP.SetActive(true);
+    }
+    public void NameYes()
+    {
+        PlayerPrefs.SetString("name",nameText.text);
+        SceneManager.LoadScene("mainScene");
+    }
+}
